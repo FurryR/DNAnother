64e8314e49123afc72ce1fad24007f8f0d16fee5
Fox Awa <awa_the_fox@qq.com>
2021-08-21 21:15:48 +0800

    紧急修复：修改安装的软件包

--- start.sh
+++ start.sh
@@ -4,8 +4,8 @@
 echo "正在安装需要的组件，请稍等。"
 sudo apt update
 sudo apt upgrade
-sudo apt install python3 git brotli android-sdk-libsparse-utils zip unzip -y
-sudo pip3 install common protobuf six bsdiff4 lzma
+sudo apt install python3 git brotli android-sdk-libsparse-utils zip unzip python3-pip -y
+sudo pip3 install common protobuf six bsdiff4
 echo "正在下载组件..."
 git clone https://github.com/awathefox/DNAnother.git
 cd DNAnother
