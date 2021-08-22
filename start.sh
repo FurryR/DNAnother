2906c4fa4604fe2bc64332bfa92f1ab220c46e6e
Fox Awa <awa_the_fox@qq.com>
2021-08-22 15:02:00 +0800

    紧急修复：添加abootimg

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 echo "正在安装需要的组件，请稍等。"
 sudo apt update
 sudo apt upgrade
-sudo apt install dialog python3 git brotli android-sdk-libsparse-utils zip unzip python3-pip -y
+sudo apt install abootimg dialog python3 git brotli android-sdk-libsparse-utils zip unzip python3-pip -y
 sudo pip3 install common protobuf six bsdiff4
 echo "正在下载组件..."
 git clone https://github.com/awathefox/DNAnother.git
